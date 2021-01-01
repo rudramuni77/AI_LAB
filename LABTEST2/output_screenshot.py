592202ec364ca5747622e4261c05f9cce6adaacd
rudramuni77 <53811238+rudramuni77@users.noreply.github.com>
2021-01-01 16:24:16 +0530

    Add files via upload

---
+++ LABTEST2/output_screenshot.py
@@ -0,0 +1,9 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: C:\Users\DELL\Desktop\ai2.py ====================
+Enter FOL:
+∀xgardener(x)=>likes(x,sun)
+The CNF form of the given FOL is: 
+~gardener(A)Vlikes(A,sun)
+>>> 
